b8af7e7f1d88d0e87165e269328db449488a38b8
Karin Karlsson <karin.drake@gmail.com>
2020-08-20 07:57:34 +0200

    fram till övning 7b, params

--- server.js
+++ server.js
@@ -3,35 +3,57 @@ const app=express()
 
 let port=1337;
 
-app.get('/', (req, res)=>{
-    console.log("I get")
 
+//ROUTES*************
+app.get('/', (req, res)=>{
+    console.log("GET /")
     res.send("I never did mind about the little things.")
 })
 
-//övning 2
+//*övning 2
 app.get('/hello', (req, res)=>{
-    console.log('GET /')
+    console.log('GET /hello')
     res.send("Detta är övning 2: Hello world.")
 })
 
-//övning 3
-
+//*övning 3
 app.get('/index.html', (req, res)=>{
     console.log('GET /index')
     res.sendFile(__dirname + '/index.html')
 
 })
 
-//Övning 4
-
-let fruits = ['strawberry', 'pear', 'apple', 'banana', 'orange']
+//*Övning 4*****************plocka ut valt objekt
+let fruits = ['strawberry', 'rambutan', 'chayote', 'ackee', 'dragon fruit']
 
 app.get('/fruits', (req, res)=>{
-    console.log("Frukten är: ", res)
+    console.log("GET /fruits", req.query)
+    //Request är ett objekt som innehåller ett queryobjekt, alltså går det att plocka ut egenskaper ur queryobjektet.
+    let index=req.query.index
     res.send(fruits[index])
 })
 
+//*Övning 7a******************lägg till nytt objekt
+//i url skriv: localhost:1337/fruits/add?name=nyfrukt
+
+app.get('/fruits/add', (req,res)=>{
+    console.log("GET /add")
+    let name=req.query.name
+    fruits.push(name);
+    res.send("La till:" + name)
+})
+
+//*Övning 7b*******************params
+//i url skriv: localhost:1337/fruits/add/nyfrukt
+
+app.get('/fruits/add/:newFruit', (req, res)=>{
+    console.log('GET fruits/add/:newFruit'+ req.params)
+    let name=req.params.newFruit;
+    fruits.push(name)
+    res.send('Added with params: '+ name)
+})
+
+//START SERVER********************
 app.listen(port, ()=>{
     console.log("Server listening on port: ", port)
 })
